863f80415c028cf3fcc0838332f6971d862fbd9b
camalot <camalot@gmail.com>
2018-03-17 13:50:32 -0500

    debugging

--- scripts/check-speed.sh
+++ scripts/check-speed.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 
 set -e;
+
+
+
+get_opts() {
+	while getopts ":n:v:o:f" opt; do
+	  case $opt in
+			a) export opt_action="$OPTARG";
+			;;
+			\?) echo "Invalid option -$OPTARG" >&2;
+			exit 1;
+			;;
+		esac;
+	done;
+
+	return 0;
+};
+
+get_opts "$@";
+
+ACTION="${opt_action:-"download"}";
+
 DATA=$(docker run --rm camalot/speedtest-cli --simple);
+(>&2 echo "$DATA");
 DOWNLOAD=$(echo $DATA | awk '/(Download:\s+)([0-9]+\.[0-9]+)\s(Mbit\/s)/ {print $2}');
+(>&2 echo "Download: $DATA Mbit/s");
 UPLOAD=$(echo $DATA | awk '/(Upload:\s+)([0-9]+\.[0-9]+)\s(Mbit\/s)/ {print $2}');
+(>&2 echo "Upload: $DATA Mbit/s");
 PING=$(echo $DATA | awk '/(Ping:\s+)([0-9]+\.[0-9]+)\s(ms)/ {print $2}');
+(>&2 echo "Ping: $PING ms");
 RESULT=0;
 MESSAGE="OK: Ping: $PING ms | Download: $DOWNLOAD Mbit/s | Upload: $UPLOAD Mbit/s";
 
@@ -35,5 +60,8 @@ elif [ $(echo $PING'>'$PMID | bc -l) == 1 ]; then
 	MESSAGE="WARNING: Ping is above $PMID ms (Ping: $PING ms | Download: $DOWNLOAD Mbit/s | Upload: $UPLOAD Mbit/s)";
 fi
 
-echo "$RESULT:$SPEED:$MESSAGE";
-
+if [ "$ACTION" == "download" ]; then
+	echo "$RESULT:$DOWNLOAD:$MESSAGE";
+else
+	echo "$RESULT:$UPLOAD:$MESSAGE";
+fi
